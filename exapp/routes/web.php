9fe653d8a6b0ee957b0692da1ddf44d994676e8d
SeiKo093 <jcruz_20@alu.uabcs.mx>
2023-11-06 03:16:12 -0600

    first commit

---
+++ exapp/routes/web.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+
+//BASE
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//SALUDO EN VISTA
+Route::get('/saluvista/{nom?}', function ($nom="") {
+    return view('saludoPlantilla', ['nom' => $nom]);
+})->where(['nom' => '[a-z]+']);
+
+//SALUDO NORMAL
+Route::get('/salunormal/{nom?}/{ape?}', function (string $nom, $ape="") {
+    return 'Te saludo '.$nom.' '.$ape;
+})->where(['nom' => '[a-z]+', 'ape' => '[a-z]+']);
+
+//CALCULADORA - SUMA
+Route::get('/sum/{num1}/{num2}', function ($num1,$num2) {
+    return $num1. ' + '.$num2. ' es = '.$num1 + $num2;
+})->where(['num1' => '[0-9]+', 'num2' => '[0-9]+']);
+
+//RESTA
+Route::get('/res/{num1}/{num2}', function ($num1,$num2) {
+    return $num1. ' - '.$num2. ' es = '.$num1 - $num2;
+})->where(['num1' => '[0-9]+', 'num2' => '[0-9]+']);
+
+//DIVISION
+Route::get('/div/{num1}/{num2}', function ($num1,$num2) {
+    return $num1. ' / '.$num2. ' es = '.$num1 / $num2;
+})->where(['num1' => '[0-9]+', 'num2' => '[0-9]+']);
+
+//MULTIPLICACION
+Route::get('/multi{num1}/{num2}', function ($num1,$num2) {
+    return $num1. ' * '.$num2. ' es = '.$num1 * $num2;
+})->where(['num1' => '[0-9]+', 'num2' => '[0-9]+']);
